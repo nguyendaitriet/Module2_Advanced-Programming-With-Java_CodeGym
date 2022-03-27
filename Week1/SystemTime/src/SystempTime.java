f6cdf8b580244581e597b81fa53b1a1328edf12e
nguyendaitriet <triet9510@gmail.com>
2022-03-27 20:55:17 +0700

    Done

---
+++ Week1/SystemTime/src/SystempTime.java
@@ -0,0 +1,9 @@
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+public class SystempTime {
+    public static void main(String[] args) {
+        Date present = new Date();
+        System.out.println("Current date-time is: " + present);
+    }
+}
