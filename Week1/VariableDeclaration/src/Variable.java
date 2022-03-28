bf83aaab638d2f35f51e9665dd38ddf8484efee7
nguyendaitriet <triet9510@gmail.com>
2022-03-28 09:42:41 +0700

    Completed

---
+++ Week1/VariableDeclaration/src/Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+    public static void main (String[] args) {
+        int i = 132;
+        float f = -10.945f;
+        double d = 40.76;
+        boolean b = false;
+        char c = '%';
+        String s = "Viet Nam";
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+
+    }
+}
