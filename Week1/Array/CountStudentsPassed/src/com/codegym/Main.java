ec8cc86f1f527ee6f928da2bec5e2b7fcab531cf
nguyendaitriet <triet9510@gmail.com>
2022-04-01 09:39:37 +0700

    Done

--- Week1/Array/CountStudentsPassed/src/com/codegym/Main.java
+++ Week1/Array/CountStudentsPassed/src/com/codegym/Main.java
@@ -15,11 +15,20 @@ public class Main {
         double[] markArray = new double[quantity];
         int count = 0;
         for (int i = 0; i < quantity; i++) {
-            markArray[i] = Math.abs(input.nextDouble());
+            do {
+                markArray[i] = input.nextDouble();
+                if (markArray[i] < 0 || markArray[i] > 10) {
+                    System.out.println("Invalid mark, please enter again!");
+                } else {
+                    break;
+                }
+            } while (true);
             if (markArray[i] >= 5) {
                 count++;
             }
         }
+
+        System.out.printf("Your entered mark: %s \n", Arrays.toString(markArray));
         if (count == 0) {
             System.out.println("There is no student passed the test.");
         } else {
