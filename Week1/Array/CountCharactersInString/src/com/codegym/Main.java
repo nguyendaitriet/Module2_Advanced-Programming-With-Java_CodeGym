96d9d2d3793f5a5ba127f397b9d536950ce564b3
nguyendaitriet <triet9510@gmail.com>
2022-04-01 08:56:15 +0700

    Done

--- Week1/Array/CountCharactersInString/src/com/codegym/Main.java
+++ Week1/Array/CountCharactersInString/src/com/codegym/Main.java
@@ -9,16 +9,18 @@ public class Main {
         System.out.print("Enter a string: ");
         String myString = input.nextLine();
         System.out.print("Enter a character you want to find: ");
-        String myChar = input.nextLine();
+        char myChar = input.next().charAt(0);
 
         int count = 0;
         for (int i = 0; i < myString.length(); i++) {
-            if (myChar.equals(myString.charAt(i))) {
+            if (myChar == myString.charAt(i)) {
                 count++;
             }
         }
         if (count == 0) {
             System.out.printf("Can't find '%s' in your string!", myChar);
+        } else {
+            System.out.printf("There %s %d character '%s' in your string.", count == 1 ? "is" : "are", count, myChar);
         }
     }
 }
