947d824b155ef903dae9e3d18612b6ed329e3171
nguyendaitriet <triet9510@gmail.com>
2022-04-12 15:01:21 +0700

    Done

---
+++ Week2/Inheritance/Circle/src/ComparableCircle.java
@@ -0,0 +1,15 @@
+public class ComparableCircle extends Circle implements Comparable {
+
+    public ComparableCircle() {}
+
+    public ComparableCircle(double radius, String color) {
+        super(color,radius);
+    }
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
+    }
+}
--- Week2/Inheritance/Circle/src/Test.java
+++ Week2/Inheritance/Circle/src/Test.java
@@ -3,5 +3,8 @@ public class Test {
         Cylinder myCylinder = new Cylinder("blue",9.8,3.3);
         System.out.println(myCylinder.getCircleRadius());
         System.out.println(myCylinder.getVolume());
+        ComparableCircle myCircle1 = new ComparableCircle(5.6,"blue");
+        ComparableCircle myCircle2 = new ComparableCircle(5.6,"brown");
+        System.out.println(myCircle1.compareTo(myCircle2));
     }
 }
